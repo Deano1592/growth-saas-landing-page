46bc99258c226c902d479a312c69c510202eea13
Dean Ridgwell <deano1592@hotmail.com>
2024-10-31 14:33:13 +0000

    JS for the FAQ section

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+// FAQ ACCORDION
+
+document.addEventListener("DOMContentLoaded", () => {
+  const faqContainer = document.querySelector(".faq-content");
+
+  faqContainer.addEventListener("click", (e) => {
+    console.log(e.target);
+  });
+});
