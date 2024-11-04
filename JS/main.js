93b847c4f1c96bab066f13d163272304c2dd2caf
Dean Ridgwell <deano1592@hotmail.com>
2024-11-04 13:57:24 +0000

    JS for the FAQ section

--- JS/main.js
+++ JS/main.js
@@ -4,6 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const faqContainer = document.querySelector(".faq-content");
 
   faqContainer.addEventListener("click", (e) => {
-    console.log(e.target);
+    const groupHeader = e.target.closest(".faq-group-header");
+
+    if (!groupHeader) return;
+
+    const group = groupHeader.parentElement;
+    const groupBody = group.querySelector(".faq-group-body");
+    const icon = groupHeader.querySelector("i");
+
+    // TOGGLE ICON
+    icon.classList.toggle("fa-plus");
+    icon.classList.toggle("fa-minus");
   });
 });
